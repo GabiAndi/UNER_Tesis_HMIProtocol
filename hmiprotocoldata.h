79aeb63de58aeed6b0b57dac0dd0b138408aa852
GabiAndi <gabiandiagui@gmail.com>
2022-02-26 17:59:40 -0300

    Añadido el comando de recepción de datos a distancia

--- hmiprotocoldata.h
+++ hmiprotocoldata.h
@@ -13,13 +13,42 @@
 
 namespace hmiprotocoldata
 {
+    union DataConverter
+    {
+        uint8_t u8[8];
+        uint16_t u16[4];
+        uint32_t u32[2];
+        uint64_t u64;
+
+        float f[2];
+        double d;
+    };
+
     enum Command : uint8_t
     {
+        GET_PARAM = 0x00,
+        REQUEST_GET_PARAM = GET_PARAM,
+        SET_PARAM = 0x50,
+        REQUEST_SET_PARAM = SET_PARAM,
         KEEP_ALIVE = 0xA0,
         LOGIN = 0xA1,
         FORCE_LOGIN = 0xA2,
         LOGIN_REQUEST = 0xA3,
         DISCONNECT_CODE = 0xA4,
+    };
+
+    enum Sensor : uint8_t
+    {
+        SENSOR_LV_FOSO = 0x00,
+        SENSOR_LV_LODO = 0x01,
+        SENSOR_TEMP = 0x02,
+        SENSOR_OD = 0x03,
+        SENSOR_PH_ANOX = 0x04,
+        SENSOR_PH_AIREACION = 0x05,
+        SENSOR_MOTOR_CURRENT = 0x10,
+        SENSOR_MOTOR_VOLTAJE = 0x11,
+        SENSOR_MOTOR_TEMP = 0x12,
+        SENSOR_MOTOR_VELOCITY = 0x13,
     };
 
     enum KeepAliveMode : uint8_t
